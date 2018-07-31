06f100d7be9e48949611d1189ab76db9697fe8d3
Stroitelev Artem <stroitelev61@gmail.com>
2018-07-31 19:34:55 +0300

    security

--- auth-service/src/main/java/ru/hf/repository/UserRepository.java
+++ auth-service/src/main/java/ru/hf/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package ru.hf.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.hf.model.User;
 
@@ -9,7 +11,10 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findOneByUsername(String username);
+    @Query("SELECT DISTINCT user FROM User user " +
+            "INNER JOIN FETCH user.authorities AS authorities " +
+            "WHERE user.username = :username")
+    User findByUsername(@Param("username") String username);
 
     @Override
     Optional<User> findById(Long id);
---
+++ auth-service/src/main/java/ru/hf/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.hf.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.hf.model.User;
+import ru.hf.repository.UserRepository;
+
+@Service(value = "userService")
+public class UserServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return user;
+        }
+        throw new UsernameNotFoundException(username);
+    }
+}
