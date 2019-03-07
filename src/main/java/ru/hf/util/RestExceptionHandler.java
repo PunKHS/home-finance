f5d286241d1874f5ee56e66dc136212762469b25
PunKHS <stroitelev61@gmail.com>
2019-03-07 20:41:26 +0300

    logging

--- src/main/java/ru/hf/util/RestExceptionHandler.java
+++ src/main/java/ru/hf/util/RestExceptionHandler.java
@@ -5,8 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.logging.Logger;
+
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    private final static Logger logger = Logger.getLogger(String.valueOf(RestExceptionHandler.class));
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(NotFoundException exc) {
@@ -14,6 +18,7 @@ public class RestExceptionHandler {
         error.setStatus(HttpStatus.NOT_FOUND.value());
         error.setMessage(exc.getMessage());
         error.setTimeStamp(System.currentTimeMillis());
+        logger.warning("status: " + error.getStatus() + " message: " + error.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
@@ -23,6 +28,7 @@ public class RestExceptionHandler {
         error.setStatus(HttpStatus.BAD_REQUEST.value());
         error.setMessage(exc.getMessage());
         error.setTimeStamp(System.currentTimeMillis());
+        logger.severe("status: " + error.getStatus() + " message: " + error.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
